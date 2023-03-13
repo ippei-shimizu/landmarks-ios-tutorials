34807cd305d4ac0253f64e506429f5c944477266
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-03-13 18:03:55 +0900

    Initial Commit

---
+++ landmarks-ios-tutorials/landmarks_ios_tutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarks_ios_tutorialsApp.swift
+//  landmarks-ios-tutorials
+//
+//  Created by Ippei on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct landmarks_ios_tutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
